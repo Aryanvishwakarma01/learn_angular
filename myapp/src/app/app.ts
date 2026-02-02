a0b72abb131c6e0f043e4aad03215944a06faa05
Aryan Vishwakarma <aryanvish86@gmail.com>
2026-02-02 16:54:59 +0000

    first commit

---
+++ myapp/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('angular_tutorial');
+  name:String = "Aryan"
+  age = 45
+}
